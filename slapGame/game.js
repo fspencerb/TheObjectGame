ded4ad8499eb42a0106e4275108080049c5c7bc9
CPink <cepinkerton.85@gmail.com>
2015-09-01 18:01:45 -0600

    finished working form of the game

--- slapGame/game.js
+++ slapGame/game.js
@@ -1,6 +1,45 @@
 var health=100;
+var name = "Teemo";
+var hits = 0;
 
 function slap()
 {
 	health-=1;
+	hits++;
+	update();
 }
+
+function punch()
+{
+	health -= 5;
+	hits++;
+	update();
+}
+
+function kick()
+{
+	health -= 10;
+	hits++;
+	update();
+}
+
+function update()
+{
+	var hp = document.getElementById('hp');
+	hp.innerText = "Health: " + health;
+	var text = document.getElementById('name');
+	text.innerText = "Name: " + name;
+	var hit = document.getElementById('hits');
+	hit.innerText = "Hits: " + hits;
+	
+	if(health <= 0)
+	{
+		alert("DEAD!");
+	}
+	
+}
+
+update();
+
+
+	
